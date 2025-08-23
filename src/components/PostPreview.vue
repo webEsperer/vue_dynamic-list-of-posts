12f2ad9a7f40fbb3f105a2a84b9f526cc03ce89f
Piotr Matejek <devmatejekpior@gmail.com>
2025-08-23 02:04:58 +0200

    fixed PostPreview

--- src/components/PostPreview.vue
+++ src/components/PostPreview.vue
@@ -12,12 +12,18 @@
       </div>
     </div>
     <p data-cy="PostBody">{{ post.body }}</p>
+
     <div class="block" v-if="isLoadingComments">
       <LoaderSpinner />
     </div>
+
     <article v-else-if="commentsError" class="message is-danger">
-      <div class="message-body">Failed to load comments. Try again later.</div>
+      <div class="message-body">
+        Failed to load comments. Try again later.
+        <button class="button is-small is-light ml-3" @click="loadComments">Retry</button>
+      </div>
     </article>
+
     <div class="block" v-else-if="comments.length > 0">
       <article
         v-for="comment in comments"
@@ -36,9 +42,11 @@
         <div class="message-body">{{ comment.body }}</div>
       </article>
     </div>
+
     <div class="block" v-else>
       <p class="title is-4">No comments yet</p>
     </div>
+
     <div class="block">
       <div v-if="isCommenting">
         <CommentForm
@@ -71,26 +79,27 @@ const isLoadingComments = ref(false)
 const commentsError = ref(false)
 const lastCommentData = ref(null)
 
+const loadComments = async () => {
+  if (!props.post) return
+  isLoadingComments.value = true
+  commentsError.value = false
+  comments.value = []
+  isCommenting.value = false
+  lastCommentData.value = null
+
+  try {
+    comments.value = await getComments(props.post.id)
+  } catch (error) {
+    console.error('Failed to load comments:', error)
+    commentsError.value = true
+  } finally {
+    isLoadingComments.value = false
+  }
+}
+
 watch(
   () => props.post,
-  async (newPost) => {
-    if (!newPost) return
-
-    isLoadingComments.value = true
-    comments.value = []
-    commentsError.value = false
-    isCommenting.value = false
-    lastCommentData.value = null
-
-    try {
-      comments.value = await getComments(newPost.id)
-    } catch (error) {
-      console.error('Failed to load comments:', error)
-      commentsError.value = true
-    } finally {
-      isLoadingComments.value = false
-    }
-  },
+  () => loadComments(),
   { immediate: true }
 )
 
@@ -105,11 +114,21 @@ const handleCommentSubmit = (newComment) => {
 }
 
 const removeComment = async (commentId) => {
+  const removedComment = comments.value.find(c => c.id === commentId)
   comments.value = comments.value.filter(c => c.id !== commentId)
+
   try {
     await deleteComment(commentId)
   } catch (error) {
     console.error('Failed to delete comment:', error)
+
+    comments.value.push(removedComment)
+
+    if (confirm('Failed to delete comment. Retry?')) {
+      removeComment(commentId)
+    }
   }
 }
+
 </script>
+
